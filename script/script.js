8514a8f9d91b7b4c4a9808d3b4d57dd411a6fa35
unknown <wasiqarmaaan00gmail.comgit config --global user.name>
2025-03-09 20:49:04 +0600

    Added index.html, index2.html, and script.js

--- script/script.js
+++ script/script.js
@@ -1,19 +1,19 @@
-// Select all buttons with the class 'completeBtn'
+
 const completeButtons = document.querySelectorAll(".completeBtn");
 const completedCount = document.getElementById("completedCount");
 const pendingCount = document.getElementById("pendingCount");
 
 
-// Function to handle button click
+
 function handleComplete(event) {
-    // Show moral alert
+    
     alert("Great job! Keep completing your tasks.");
 
-    // Convert counts to numbers
+    
     let completed = parseInt(completedCount.innerText);
     let pending = parseInt(pendingCount.innerText);
 
-    // Update values
+    
     if (pending > 0) {
         completedCount.innerText = completed + 1;
         pendingCount.innerText = pending - 1;
